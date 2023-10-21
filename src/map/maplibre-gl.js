7e19a94db8ada790744fc8b05a35eed92c7c1cc2
DavronSoftwareEngineer <davron.software.engineer@mail.ru>
2023-10-21 12:22:23 +0500

    o'zgarishlar saqlandi

--- src/map/maplibre-gl.js
+++ src/map/maplibre-gl.js
@@ -1,85 +1,98 @@
 import React, { useRef, useEffect, useState } from "react";
-import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
-import mapData from "./map.json";
-console.log(mapData);
+import maplibregl from "maplibre-gl";
 function MapLibreGl() {
   const mapContainer = useRef(null);
   const map = useRef(null);
   const [lng] = useState(69.279737);
   const [lat] = useState(41.311158);
   const [zoom] = useState(14);
-  const MAPTILER_KEY = "get_your_own_OpIi9ZULNHzrESv6T2vL";
   useEffect(() => {
-    if (map.current) return;
-    map.current = new maplibregl.Map({
-      container: mapContainer.current,
-      style: mapData,
-      center: [lng, lat],
-      zoom: zoom,
-      pitch: 80,
-      bearing: -17.6,
-      antialias: true,
-    });
+    const initializeMap = () => {
+      map.current = new maplibregl.Map({
+        container: mapContainer.current,
+        style: `http://localhost:3001/mapStyle/style.json`,
+        center: [lng, lat],
+        zoom: zoom,
+        pitch: 60,
+        bearing: -17.6,
+        antialias: true,
+      });
+    };
+
+    if (!map.current) {
+      initializeMap();
+    }
 
     map.current.on("load", () => {
-      // Insert the layer beneath any symbol layer.
-      const layers = map.current.getStyle().layers;
+      
+      map.current?.on("mouseenter", "3d-buildings", (e) => {
+        map.current.getCanvas().style.cursor = "pointer";
+        var feature = e.features[0];
+        var color = "#07257F";
 
-      let labelLayerId;
-      for (let i = 0; i < layers.length; i++) {
-        if (layers[i].type === "symbol" && layers[i].layout["text-field"]) {
-          labelLayerId = layers[i].id;
-          break;
-        }
-      }
-
-      map.current.addSource("openmaptiles", {
-        url: mapData,
-        type: "vector",
-        tiles: ['./tiles.pbf'],
+        map.current.setPaintProperty("3d-buildings", "fill-extrusion-color", [
+          "case",
+          ["==", ["id"], feature.id],
+          color,
+          [
+            "interpolate",
+            ["linear"],
+            ["get", "render_height"],
+            0,
+            "#1e88e5",
+            30,
+            "#1966d2",
+            70,
+            "#1555c0",
+            100,
+            "#0d47a1",
+          ],
+        ]);
       });
 
-      map.current.addLayer(
-        {
-          id: "3d-buildings",
-          source: "openmaptiles",
-          "source-layer": "building",
-          type: "fill-extrusion",
-          minzoom: 15,
-          paint: {
-            "fill-extrusion-color": [
-              "interpolate",
-              ["linear"],
-              ["get", "render_height"],
-              0,
-              "lightgrey",
-              200,
-              "royalblue",
-              400,
-              "lightblue",
-            ],
-            "fill-extrusion-height": [
-              "interpolate",
-              ["linear"],
-              ["zoom"],
-              15,
-              0,
-              16,
-              ["get", "render_height"],
-            ],
-            "fill-extrusion-base": [
-              "case",
-              [">=", ["get", "zoom"], 16],
-              ["get", "render_min_height"],
-              0,
-            ],
-          },
-        },
-        labelLayerId
-      );
+      map.current?.on("click", "3d-buildings", (e) => {
+        map.current.getCanvas().style.cursor = "pointer";
+        var feature = e.features[0];
+        var color = "yellow";
+        map.current?.setPaintProperty("3d-buildings", "fill-extrusion-color", [
+          "case",
+          ["==", ["id"], feature.id],
+          color,
+          [
+            "interpolate",
+            ["linear"],
+            ["get", "render_height"],
+            0,
+            "#1e88e5",
+            30,
+            "#1966d2",
+            70,
+            "#1555c0",
+            100,
+            "#0d47a1",
+          ],
+        ]);
+      });
+
+      map.current?.on("mouseout", "3d-buildings", () => {
+        map.current.getCanvas().style.cursor = "";
+        map.current?.setPaintProperty("3d-buildings", "fill-extrusion-color", [
+          "interpolate",
+          ["linear"],
+          ["get", "render_height"],
+          0,
+          "#1e88e5",
+          30,
+          "#1966d2",
+          70,
+          "#1555c0",
+          100,
+          "#0d47a1",
+        ]);
+      });
     });
-  }, [map.current]);
+  }, []);
   return (
     <div className="map-wrap">
       <div ref={mapContainer} className="map" />
